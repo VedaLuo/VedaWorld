5157801b32c4cee8251b8c8d4134caef9c9727ce
VedaLuo <luozhichong@126.com>
2018-02-02 14:13:48 +0800

    my first 001

---
+++ MyJava/src/com/lzc/test/Main.java
@@ -0,0 +1,12 @@
+package com.lzc.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world! 001");
+
+	}
+
+}
