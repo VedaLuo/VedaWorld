1eb2f603dee48f69280c20a93bce0d6187330ba6
VedaLuo <luozhichong@126.com>
2018-02-02 14:45:36 +0800

    003

--- MyJava/src/com/lzc/test/Main.java
+++ MyJava/src/com/lzc/test/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello world! 002");
+		System.out.println("Hello world! 003");
 
 	}
 
